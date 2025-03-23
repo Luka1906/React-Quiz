9d3644b56a26221b4d1ab468cd3ab80b9efefd2d
Luka Matovic <lukamatovic1994@gmail.com>
2025-03-23 00:37:27 -0500

    Refactor QuestionTimer to clear timeouts and intervals on unmount; update Quiz component to use key prop for QuestionTimer to reset it on each question change

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,18 +4,24 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
- 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
 
-  useEffect( () => {
-    setInterval(() => {
-        setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
-      }, 100);
-  }, [])
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-  
-
-  return <progress id="question-time" max={timeout} value={remainingTime}></progress>;
+  return (
+    <progress id="question-time" max={timeout} value={remainingTime}></progress>
+  );
 }
